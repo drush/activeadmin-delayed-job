261bfe001d8982af59cc3d4d0c81f88c69cf7d63
Michael Bianco <iloveitaly@gmail.com>
2014-10-10 14:57:31 -0400

    Removing scoping code that wasn't in use

--- app/models/delayed_job_decorator.rb
+++ app/models/delayed_job_decorator.rb
@@ -1,21 +1,4 @@
 Delayed::Job.class_eval do
-  class << self
-    def all
-      scoped
-    end
-
-    def running
-      where('failed_at is null and locked_at is not null')
-    end
-
-    def failed
-      where('failed_at is not null')
-    end
-
-    def waiting
-      where('failed_at is null and locked_at is null')
-    end
-  end
 
   def state
     return 'failed' unless failed_at.blank?
--- lib/active_admin_delayed_job/admin/delayed_jobs.rb
+++ lib/active_admin_delayed_job/admin/delayed_jobs.rb
@@ -5,24 +5,20 @@ ActiveAdmin.register Delayed::Job, :as => "Background Job" do
 
   filter :queue
 
-  # scope :all
-  # scope :running
-  # scope :failed
-  # scope :waiting
-
   batch_action 'Retry' do |selection|
     Delayed::Job.find(selection).each do |job|
       job.retry!
     end
+
     redirect_to admin_background_jobs_path, :notice => "Retrying jobs"
   end
-
 
   index do     
     selectable_column                        
     column :id 
     column :queue
     column :priority
+    column :attempts
     
     column :status do |job|
       case job.state
@@ -41,16 +37,6 @@ ActiveAdmin.register Delayed::Job, :as => "Background Job" do
       end
       
     end
-
-    #column :run_at
-
-    column :attempts
-
-    #column :locked_at
-    #column :locked_by
-    #column :failed_at
-    #column :last_error
-    #column :created_at
 
     actions                   
   end 
